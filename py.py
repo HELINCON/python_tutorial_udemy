ee07723359aa6bfdfeb9b6f83da4c0f8a2aa4bd2
MX1234 <mx.helincon@iserveu.co.in>
2023-08-04 12:28:53 +0530

    new

---
+++ py.py
@@ -0,0 +1,30 @@
+# patient = "john players"
+# age = 20
+# # print("user details--------->"+patient, age)
+
+# new_patient = {patient, age}
+# new_patient2 = patient,age
+# print(new_patient)   # here the answer will be {20, 'john players'}  first int then string
+# print(new_patient2)  # but here the answer will be ('john players', 20) first string then int
+
+# # name = input("what is your name")
+# # print("hello", name)
+
+# clas = input("what is your class  ")
+# print("my class is ",clas)
+
+# birth_year = input("enter the birthday year ")
+# age = 2023 - int(birth_year)
+# print("the age is",age)
+
+# side_name = "rajesh is male"
+# print(side_name.find("j"))
+# print(type(side_name))
+# print(side_name.replace(side_name, "the gender of "+ side_name))
+# print(side_name.replace("male", "a friend of frankok charlie"))
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[16:13:-1]
+print(backwards)
+newbackwards = letters[24:16:-3]
+print(newbackwards)
