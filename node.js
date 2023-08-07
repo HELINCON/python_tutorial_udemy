466be561e854859fcac3e6a69e2eaef86fecd7c5
MX1234 <mx.helincon@iserveu.co.in>
2023-08-07 11:19:12 +0530

    new

--- node.js
+++ node.js
@@ -3,4 +3,6 @@ let age = 20
 let new_patient = {patient,age}
 
 console.log(new_patient);
+console.log("the type of age is ==> ", typeof age);
 
+console.log('We can even use double "quotes" in string');
--- py.py
+++ py.py
@@ -1,30 +1,31 @@
-# patient = "john players"
-# age = 20
-# # print("user details--------->"+patient, age)
+patient = "john players"
+age = 20
+# print("user details--------->"+patient, age)
 
-# new_patient = {patient, age}
-# new_patient2 = patient,age
-# print(new_patient)   # here the answer will be {20, 'john players'}  first int then string
-# print(new_patient2)  # but here the answer will be ('john players', 20) first string then int
+new_patient = {patient, age}
+new_patient2 = patient,age
+print(new_patient)   # here the answer will be {20, 'john players'}  first int then string
+print(new_patient2)  # but here the answer will be ('john players', 20) first string then int
 
-# # name = input("what is your name")
-# # print("hello", name)
+# name = input("what is your name")
+# print("hello", name)
 
-# clas = input("what is your class  ")
+clas = input("what is your class  ")
 # print("my class is ",clas)
 
-# birth_year = input("enter the birthday year ")
-# age = 2023 - int(birth_year)
-# print("the age is",age)
+birth_year = input("enter the birthday year ")
+age = 2023 - int(birth_year)
+print("the age is",age)
 
-# side_name = "rajesh is male"
-# print(side_name.find("j"))
-# print(type(side_name))
-# print(side_name.replace(side_name, "the gender of "+ side_name))
-# print(side_name.replace("male", "a friend of frankok charlie"))
+side_name = "rajesh is male"
+print(side_name.find("j"))
+print("the data type of side name is ==> " + type(side_name))  
+# string cannot concatenate with type of in python 
+print(side_name.replace(side_name, "the gender of "+ side_name))
+print(side_name.replace("male", "a friend of frankok charlie"))
 
-letters = "abcdefghijklmnopqrstuvwxyz"
-backwards = letters[16:13:-1]
-print(backwards)
-newbackwards = letters[24:16:-3]
-print(newbackwards)
+# letters = "abcdefghijklmnopqrstuvwxyz"
+# backwards = letters[16:13:-1]
+# print(backwards)
+# newbackwards = letters[24:16:-3]
+# print(newbackwards)
