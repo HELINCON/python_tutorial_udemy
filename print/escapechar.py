ff24a8244226ebebe30b6ffeb28310fa6df8c660
MX1234 <mx.helincon@iserveu.co.in>
2023-08-07 11:17:47 +0530

    added

---
+++ print/escapechar.py
@@ -0,0 +1,5 @@
+splitString =  "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+tabbedstring = "1\t2\t3\t4\t5"
+print(tabbedstring)
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
